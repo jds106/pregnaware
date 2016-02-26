d7ac078bd59c6edb45682f5a14a53875dcf19723
James Seymour <james@jamesseymour.co.uk>
2016-02-26 09:13:49 +0000

    Script fix

--- admin/update.sh
+++ admin/update.sh
@@ -6,9 +6,21 @@ BUILD_DIR=~/build
 DIST_DIR=~/dist
 
 # Build
-cd ~/build/pregnaware/progresssvc
+cd $DIST_DIR/pregnaware/progresssvc
 sbt assembly
+JAR_FILE=`ls target/scala-2.11/*.jar`
+cp $JAR_FILE $DIST_DIR/bin/
+echo "Copied new JAR file: $JAR_FILE"
 
+# Update web files
 cd ~/dist
 cp -r ~/build/pregnaware/frontend/www ~/dist/
-cd 
+cd ~/dist/www/js
+npm install
+echo "Updated website"
+
+echo
+echo "######################"
+echo "# Pregnaware updated #"
+echo "######################"
+echo

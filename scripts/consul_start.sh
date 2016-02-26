69546cc6282da15f19dcbb49974a2309d10d939d
James Seymour <james@jamesseymour.co.uk>
2016-02-26 09:50:49 +0000

    Script fix

--- scripts/consul_start.sh
+++ scripts/consul_start.sh
@@ -6,6 +6,7 @@ ROOT_DIR=~/dist
 ARGS="-bootstrap -server -dev"
 ARGS="$ARGS -config-dir=$ROOT_DIR/etc/consul"
 ARGS="$ARGS -data-dir=$ROOT_DIR/data/consul"
+ARGS="$ARGS -pid-file=$ROOT_DIR/log/consul.pid.log"
 ARGS="$ARGS -ui -ui-dir=$ROOT_DIR/bin/consului"
 
 nohup $CONSUL_BIN agent $ARGS  > $ROOT_DIR/log/consul.log 2>&1 &

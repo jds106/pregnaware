d27acd6b64b933345c0ad4fdd319edd1e1622aad
James Seymour <james@jamesseymour.co.uk>
2016-03-01 19:12:58 +0000

    Friend requests

---
+++ service/src/main/scala/pregnaware/database/wrappers/userwrappers/DueDateWrapper.scala
@@ -0,0 +1,8 @@
+package pregnaware.database.wrappers.userwrappers
+
+/**
+  * Created by james on 01/03/2016.
+  */
+trait DueDateWrapper {
+
+}
---
+++ service/src/main/scala/pregnaware/user/entities/WrappedFriendToBe.scala
@@ -0,0 +1,8 @@
+package pregnaware.user.entities
+
+/**
+  * Created by james on 01/03/2016.
+  */
+class WrappedFriendToBe {
+
+}

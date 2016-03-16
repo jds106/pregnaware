03c26eba545604917712d7f7cf3c7e8ce87d0494
jamess <james@jamesseymour.co.uk>
2016-03-16 18:00:30 +0000

    Added request / response logging

--- service/src/main/scala/pregnaware/frontend/FrontEndHttpService.scala
+++ service/src/main/scala/pregnaware/frontend/FrontEndHttpService.scala
@@ -1,12 +1,15 @@
 package pregnaware.frontend
 
 import akka.actor.{ActorContext, ActorRefFactory}
+import akka.event.Logging._
 import akka.util.Timeout
 import com.typesafe.scalalogging.StrictLogging
 import pregnaware.frontend.services.naming.{NamingServiceBackend, NamingServiceFrontEnd}
 import pregnaware.frontend.services.user.{UserServiceBackend, UserServiceFrontEnd}
 import pregnaware.utils.ExecutionWrapper
+import spray.http.{HttpResponse, HttpRequest}
 import spray.routing._
+import spray.routing.directives.LogEntry
 
 import scala.concurrent.ExecutionContext
 
@@ -20,7 +23,11 @@ abstract class FrontEndHttpService extends HttpService
   /** The routes defined by this service */
   val routes =
     pathPrefix(FrontEndHttpService.serviceName) {
-      userServiceRoutes ~ namingServiceRoutes
+      logRequest("REST API", InfoLevel) {
+        logResponse("REST API", InfoLevel) {
+          userServiceRoutes ~ namingServiceRoutes
+        }
+      }
     }
 }
 
@@ -29,20 +36,24 @@ object FrontEndHttpService {
 
   def apply(
     persistence: SessionPersistence, userSvc: UserServiceBackend, namingSvc: NamingServiceBackend)
-    (implicit ac: ActorContext, ec: ExecutionContext, to: Timeout) : FrontEndHttpService = {
+    (implicit ac: ActorContext, ec: ExecutionContext, to: Timeout): FrontEndHttpService = {
 
     new FrontEndHttpService {
 
       // Needed for ExecutionWrapper
       implicit override final def context: ActorContext = ac
+
       implicit override final def executor: ExecutionContext = ec
+
       implicit override final def timeout: Timeout = to
 
       // Needed for HttpService
       implicit override final def actorRefFactory: ActorRefFactory = ac
 
       override def getSessionPersistence = persistence
+
       override def getUserService = userSvc
+
       override def getNamingService = namingSvc
     }
   }
